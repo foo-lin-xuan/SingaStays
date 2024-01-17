33c69bde72ac94727307a7cb0f8eaa7f41e52648
Poh YH <pohyh@stakewith.us>
2024-01-17 09:34:59 +0800

    Edit Index.js to include wrapped UserContextProvider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import { UserContextProvider } from "./Components/UserContextProvider";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { UserContextProvider } from './Components/UserContextProvider';
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
-    <UserContextProvider>
-      <App />
+  <React.StrictMode>
+     <UserContextProvider>
+    <App />
     </UserContextProvider>
-  </StrictMode>
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
